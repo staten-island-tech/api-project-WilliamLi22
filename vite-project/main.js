70815a650551dd2a46bd650400b88020235c8e19
WilliamLi22 <forvalious@gmail.com>
2023-01-12 10:16:37 -0500

    Found api, now to start

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,1 +1,19 @@
 import "./style.css";
+const URL = "https://www.balldontlie.io/api/v1/players";
+let player = "bensimmons";
+
+
+
+async function getData(URL) {
+    try {
+        const response = await fetch(URL);
+        if (response.status < 200 || response.status > 299) {
+            console.log(response.status);
+            throw error (response);
+        } else {
+            const data = await response.json();
+            data.data[0].entry.forEach()
+        }
+
+    }
+}
