03d908986c93155307dc47a8f11680d2397d4a4c
WilliamLi22 <forvalious@gmail.com>
2023-01-19 10:16:40 -0500

    failinmg

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,28 +1,28 @@
 import "./style.css";
+
 const URL = "https://www.balldontlie.io/api/v1/players";
 
 async function getData(URL) {
   try {
     const response = await fetch(URL);
     if (response.status < 200 || response.status > 299) {
-      console.log(response.status);
-      throw error(response);
+      throw new error(response);
     } else {
       const data = await response.json();
-      data.forEach((player) => {
+      data.data.forEach((player) => {
         document.getElementById("api-reponse").insertAdjecentHTML(
           "afterbegin",
           `<div class ="card">
                 <h2 class = "title">${player.first_name}</h2>
+                <h2 class = "title">${player.position}</h2>
+                <h2 class = "title">${player.team.full_name}</h2>
                 </div>`
         );
       });
-      console.log(data.data[0].first_name);
+      console.log("ok");
     }
   } catch (error) {
     console.log("error");
-    document.getElementById("api - response").textcontent =
-      "I'm sorry, I couldn't find that. Please try again.";
   }
 }
 
