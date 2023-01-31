8f907451c50e1c2eca4cb639c8d1a8b674ea6fab
WilliamLi22 <forvalious@gmail.com>
2023-01-30 23:20:06 -0500

    Not api-ing

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,4 +1,31 @@
-import "./style.css";
+import "../styles/style.css";
 import { DOMSelectors } from "./dom";
 
-const URL = "https://pokeapi.co/api/v2/pokemon";
+const apiEntry = `https://www.balldontlie.io/api/v1/players`;
+
+async function getAPI() {
+  try {
+    const result = await fetch(`https://www.balldontlie.io/api/v1/players`);
+    if (response.status < 200 || response.status > 299) {
+      throw Error(response.status);
+    } else {
+      const players = await result.json();
+      return players;
+    }
+  } catch (err) {
+    console.log("Error");
+    document.getElementById("api-response").textContent =
+      "I am sorry, there is an error.";
+  }
+}
+
+/* async function getApi() {
+  try {
+    const players = await fetch(`https://www.balldontlie.io/api/v1/players`);
+    console.log(players);
+  } catch (error) {
+    console.log(error);
+  }
+}
+getApi();
+ */
