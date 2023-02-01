d84f5d5a6908e467b5bdb2ffcdb8edc51c381b43
WilliamLi22 <forvalious@gmail.com>
2023-01-31 21:41:53 -0500

    Data undefined?

--- vite-project/js/dom.js
+++ vite-project/js/dom.js
@@ -1,3 +1,11 @@
-const DOMSelectors = {};
+const DOMSelectors = {
+  apiresponse: document.getElementById("apiresponse"),
+  all: document.getElementById("all"),
+  pointguards: document.getElementById("pointguards"),
+  shootingguards: document.getElementById("shootingguards"),
+  smallforwards: document.getElementById("smallforwards"),
+  powerforwards: document.getElementById("powerforwards"),
+  centers: document.getElementById("centers"),
+};
 
 export { DOMSelectors };
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -6,10 +6,43 @@ const URL = `https://www.balldontlie.io/api/v1/players`;
 async function getData(URL) {
   try {
     const response = await fetch(URL);
-    const data = await response.json(); //makes the data into json object we can use
-    console.log(data.first_name);
+    if (response.status < 200 || response.status > 299) {
+      throw Error(response.status);
+    } else {
+      const data = await response.json(); //makes the data into json object we can use
+      console.log(data);
+      document.getElementById("api-response");
+    }
   } catch (error) {
     console.log(error);
   }
 }
 getData(URL);
+
+const apiresponse = document.querySelector(`#apiresponse`);
+
+document.getElementById("all").addEventListener("click", function () {
+  apiresponse.innerHTML = ``;
+  allplayers();
+});
+
+async function allplayers() {
+  const response = await fetch(URL);
+  const Info = await response.json();
+  console.log(Info);
+  data
+    .filter((data) => data.includes("team"))
+    .forEach((data) =>
+      document.getElementById("apiresponse").insertAdjacentHTML(
+        "beforeend",
+        `
+        <div class="card">
+        <h2 class="all">${data.first_name} ${data.last_name}</h2>
+        <h2 class="all">${data.height_feet} ${data.height_inches}</h2>
+        <h2 class="all">${data.weight}</h2>
+ 
+  </div>
+  `
+      )
+    );
+}
