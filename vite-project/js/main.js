bbfe4b1f73b4171fc00d419532f004a3faf327fd
WilliamLi22 <forvalious@gmail.com>
2023-01-31 19:29:46 -0500

    Fixing

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,31 +1,15 @@
 import "../styles/style.css";
 import { DOMSelectors } from "./dom";
 
-const apiEntry = `https://www.balldontlie.io/api/v1/players`;
+const URL = `https://www.balldontlie.io/api/v1/players`;
 
-async function getAPI() {
+async function getData(URL) {
   try {
-    const result = await fetch(`https://www.balldontlie.io/api/v1/players`);
-    if (response.status < 200 || response.status > 299) {
-      throw Error(response.status);
-    } else {
-      const players = await result.json();
-      return players;
-    }
-  } catch (err) {
-    console.log("Error");
-    document.getElementById("api-response").textContent =
-      "I am sorry, there is an error.";
-  }
-}
-
-/* async function getApi() {
-  try {
-    const players = await fetch(`https://www.balldontlie.io/api/v1/players`);
-    console.log(players);
+    const response = await fetch(URL);
+    const data = await response.json(); //makes the data into json object we can use
+    console.log(data.first_name);
   } catch (error) {
     console.log(error);
   }
 }
-getApi();
- */
+getData(URL);
