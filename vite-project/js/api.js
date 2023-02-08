24f42a675abfaf0286833c52fb74f04c557279fa
WilliamLi22 <forvalious@gmail.com>
2023-02-08 09:38:45 -0500

    e

--- vite-project/js/api.js
+++ vite-project/js/api.js
@@ -1,6 +1,26 @@
-const URL = [
-  `https://www.balldontlie.io/api/v1/players?per_page=100`,
-  `https://www.balldontlie.io/api/v1/players?page=2&per_page=100`,
+const URL = [`https://www.balldontlie.io/api/v1/players?per_page=100`];
+
+//https://www.balldontlie.io/api/v1/players?page=2&per_page=100 use this and continue to do for all pages
+
+async function getData(URL) {
+  try {
+    const response = await fetch(URL);
+    if (response.status < 200 || response.status > 299) {
+      throw Error(response.status);
+    } else {
+      const data = await response.json(); //makes the data into json object we can use
+      console.log(data);
+      document.getElementById("api-response");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+getData(URL);
+
+export { URL };
+
+/* `https://www.balldontlie.io/api/v1/players?page=2&per_page=100`,
   `https://www.balldontlie.io/api/v1/players?page=3&per_page=100`,
   `https://www.balldontlie.io/api/v1/players?page=4&per_page=100`,
   `https://www.balldontlie.io/api/v1/players?page=5&per_page=100`,
@@ -38,25 +58,4 @@ const URL = [
   `https://www.balldontlie.io/api/v1/players?page=36&per_page=100`,
   `https://www.balldontlie.io/api/v1/players?page=37&per_page=100`,
   `https://www.balldontlie.io/api/v1/players?page=38&per_page=100`,
-  `https://www.balldontlie.io/api/v1/players?page=39&per_page=100`,
-];
-
-//https://www.balldontlie.io/api/v1/players?page=2&per_page=100 use this and continue to do for all pages
-
-async function getData(URL) {
-  try {
-    const response = await fetch(URL);
-    if (response.status < 200 || response.status > 299) {
-      throw Error(response.status);
-    } else {
-      const data = await response.json(); //makes the data into json object we can use
-      console.log(data);
-      document.getElementById("api-response");
-    }
-  } catch (error) {
-    console.log(error);
-  }
-}
-getData(URL);
-
-export { URL };
+  `https://www.balldontlie.io/api/v1/players?page=39&per_page=100`, */
